d8ab54ab22a5adeb7bf5cd644090adf256d0f43c
Alden Lamp <alamp9008@gmail.com>
2017-01-19 18:45:51 -0500

    renaming folder

---
+++ Amigo de Reciclaje/La Guia De Estudias/SecondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondViewController.swift
+//  La Guia De Estudias
+//
+//  Created by alden lamp on 1/19/17.
+//  Copyright © 2017 alden lamp. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    
+}
+
