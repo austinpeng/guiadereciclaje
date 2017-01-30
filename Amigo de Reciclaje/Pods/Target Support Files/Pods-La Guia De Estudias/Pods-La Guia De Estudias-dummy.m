0277e9561a55c3ce94f4ce8deb177e5cda311b55
Austin Peng <austin@Austins-MacBook-Pro.local>
2017-01-30 10:27:05 -0500

    deleted old amigo

--- Amigo de Reciclaje/Pods/Target Support Files/Pods-La Guia De Estudias/Pods-La Guia De Estudias-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_La_Guia_De_Estudias : NSObject
-@end
-@implementation PodsDummy_Pods_La_Guia_De_Estudias
-@end
--- Amigo de Reciclaje/Pods/Target Support Files/Pods-La Guia De Estudias/Pods-La Guia De Estudias-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_La_Guia_De_EstudiasVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_La_Guia_De_EstudiasVersionString[];
-
